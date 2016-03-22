58bec37eee97fb04bdedb47d2d572eeda462ea8f
dquam <me@derekquam.com>
2016-03-22 15:19:14 -0500

    Statically tie to a specific API version of Vulkan
    Set the compatibility of this sample to a specific version of the Vulkan API to avoid driver incompatibilities with future Vulkan API releases.

--- vk_initlib.cpp
+++ vk_initlib.cpp
@@ -375,7 +375,7 @@ bool vulkanCreateContext(VkDevice &device, VkPhysicalDevice& physicalDevice,  Vk
   VkApplicationInfo applicationInfo = { VK_STRUCTURE_TYPE_APPLICATION_INFO };
   applicationInfo.pApplicationName = appTitle;
   applicationInfo.pEngineName = engineName;
-  applicationInfo.apiVersion = VK_API_VERSION;
+  applicationInfo.apiVersion = VK_MAKE_VERSION(1, 0, 4);
 
   // global layers and extensions
   const vector<VkLayerProperties> globalLayerProperties = GetGlobalLayerProperties();
